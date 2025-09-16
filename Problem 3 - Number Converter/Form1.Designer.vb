c1e910122ad817568cc1f6007b7f036a90cc5b64
DreGeorge <AndreiWilliamTaborda@SMU-0101>
2025-09-16 08:48:00 +0800

    Add project files.

---
+++ Problem 3 - Number Converter/Form1.Designer.vb
@@ -0,0 +1,72 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.btnConvert = New System.Windows.Forms.Button()
+        Me.txtNumber = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'lblResult
+        '
+        Me.lblResult.AutoSize = True
+        Me.lblResult.Location = New System.Drawing.Point(338, 200)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(0, 16)
+        Me.lblResult.TabIndex = 0
+        '
+        'btnConvert
+        '
+        Me.btnConvert.Location = New System.Drawing.Point(326, 142)
+        Me.btnConvert.Name = "btnConvert"
+        Me.btnConvert.Size = New System.Drawing.Size(75, 23)
+        Me.btnConvert.TabIndex = 1
+        Me.btnConvert.Text = "EWAN"
+        Me.btnConvert.UseVisualStyleBackColor = True
+        '
+        'txtNumber
+        '
+        Me.txtNumber.Location = New System.Drawing.Point(315, 101)
+        Me.txtNumber.Name = "txtNumber"
+        Me.txtNumber.Size = New System.Drawing.Size(100, 22)
+        Me.txtNumber.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.txtNumber)
+        Me.Controls.Add(Me.btnConvert)
+        Me.Controls.Add(Me.lblResult)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblResult As Label
+    Friend WithEvents btnConvert As Button
+    Friend WithEvents txtNumber As TextBox
+End Class
---
+++ Problem 3 - Number Converter/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub btnConvert_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
+        Try
+
+            Dim userInput As String = txtNumber.Text
+
+
+            Dim number As Integer = Integer.Parse(userInput)
+
+
+            lblResult.Text = "Converted Number: " & number
+
+        Catch ex As FormatException
+
+            lblResult.Text = "Invalid input. Please enter a valid number."
+        End Try
+    End Sub
+
+End Class
